83e0fc5b9e9a9468d316367f58fb1f27704cff98
bxviu <122492611+bxviu@users.noreply.github.com>
2024-12-11 15:11:34 -0800

    Revert "speed boost tower"
    This reverts commit d44490ec3160663d386fa2c2f468974db7245e9f.

--- Scenes/Player/Scripts/States/PlayerAttackState.gd
+++ Scenes/Player/Scripts/States/PlayerAttackState.gd
@@ -37,7 +37,7 @@ func deal_damage(enemy : EnemyMain):
 
 func _process(delta):
 	# Get all overlapping bodies
-	var overlapping_bodies = hitbox.get_overlapping_areas()
+	var overlapping_bodies = hitbox.get_overlapping_bodies()
 	#print(overlapping_bodies)
 	for body in overlapping_bodies:
 		if body.is_in_group("DamageBoost"):
